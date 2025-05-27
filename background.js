e90be0a1c22cb0530991c157f7671ac087cb8267
greatghoul <greatghoul@gmail.com>
2025-05-27 22:04:15 +0800

    Init Commit

---
+++ background.js
@@ -0,0 +1,21 @@
+// Create context menu item when the extension is installed
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'shareToTelegram',
+    title: 'Share to Telegram',
+    contexts: ['image']
+  });
+});
+
+// Handle context menu click
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'shareToTelegram') {
+    // Open publisher.html in a popup window
+    chrome.windows.create({
+      url: 'publisher.html',
+      type: 'popup',
+      width: 500,
+      height: 600
+    });
+  }
+});
